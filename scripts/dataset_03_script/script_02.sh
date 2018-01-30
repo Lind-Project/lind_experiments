a38a4daa8a45a17d4263da9b321e4461b3197ab6
Yiwen Li <detectivelyw@gmail.com>
2018-01-30 11:09:50 -0500

    added scripts for evaluation dataset03.

---
+++ scripts/dataset_03_script/script_02.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+cat ~/Documents/Projects/ipp/lind_experiments/data/dataset_03/pgn/*.pgn | grep "Result" | sort > ~/Documents/Projects/ipp/lind_experiments/results/dataset_03/results_02.txt
